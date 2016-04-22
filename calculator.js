2c5f0e58c4dc5139ed75e180718da2b65777cc02
tshepo <tshepo.lesejane@gmail.com>
2016-04-22 09:38:00 +0200

    String kata calculator tests done  under 10 minutes

--- calculator.js
+++ calculator.js
@@ -1,3 +1,22 @@
 exports.add = function (numbers) {
-    return numbers;
+    if(numbers=== '')
+        return 0;
+    var total = Sum(numbers)
+    return total
 };
+
+function Sum(numbers) {
+    var sum = 0;
+    if(numbers.indexOf(',') < 1){
+        return parseInt(numbers)
+    }else {
+        numbers = numbers.replace("\n", ',')
+        var numbersArray = numbers.split(',');
+
+        for(i = 0; i < numbersArray.length; i++){
+            sum += parseInt(numbersArray[i]);
+        }
+        return sum;
+    }
+
+}
--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -5,4 +5,14 @@ describe("string calculator", function () {
         var calculatorResult = calculator.add('');
         expect(calculatorResult).toBe(0);
     });
+    it("Should return the entered number if String has one number",function () {
+        expect(calculator.add("1")).toBe(1)
+    })
+    it("Should return a sum of numbers that are separated by commas on input", function () {
+        expect(calculator.add("1,2,3")).toBe(6)
+    })
+    it("Should return a sum of numbers that are separated by a new line and commas", function () {
+        expect(calculator.add("1\n2,3")).toBe(6)
+    })
+    
 });
